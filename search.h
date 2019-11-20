d105ac4d195609038a1c063f683405ede2283ea1
Krishna Sharma <53052253+delta7-138@users.noreply.github.com>
2019-11-20 19:40:24 +0530

    Add files via upload

---
+++ search.h
@@ -0,0 +1,3 @@
+void search(char namef[],char namel[],int * roll,int * fees, char date[],FILE* db);
+void call_search(char key[], char value[],char namef[],char namel[],int * roll,int * fees, char date[],int int_value,FILE* db);
+void search_driver(FILE* db);
