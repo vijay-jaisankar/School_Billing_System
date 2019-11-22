49fd4a032f11b4b9200eace111169368082e3c64
Krishna Sharma <53052253+delta7-138@users.noreply.github.com>
2019-11-23 00:06:46 +0530

    Updation of util1.c
    Addition of new function

--- util1.c
+++ util1.c
@@ -2,16 +2,13 @@
 void display()
 {
 	        printf("\nPLEASE AN CHOOSE AN ACTION FROM THE FOLLOWING LIST OF OPTIONS\n");
-                printf("|-----------WELCOME TO THIS SCHOOL BILLING SOFTWARE----------|\n");
-		printf("|	                                                     |\n");
-                printf("|         1. ADD A FIELD                                     |\n");
-                printf("|         2. DELETE A FIELD                                  |\n");
-                printf("|         3. SEARCH A FIELD                                  |\n");
-		printf("|         4. EXIT THE SOFTWARE                               |\n");
-                printf("|____________________________________________________________|\n");
+                printf("|----------------WELCOME TO THIS SCHOOL BILLING SOFTWARE----------|\n");
+		printf("|	                                                          |\n");
+                printf("|         1. ADD A RECORD                                         |\n");
+                printf("|         2. DELETE A RECORD                                      |\n");
+                printf("|         3. SEARCH A RECORD                                      |\n");
+		printf("|         4. DOMAIN BASED SEARCHING(search for a range of records)|\n");
+		printf("|         5. EXIT THE SOFTWARE                                    |\n");
+                printf("|_________________________________________________________________|\n");
 
 }
-
-
-
-
