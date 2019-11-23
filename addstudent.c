59b8204bf2c7ad5dfc91d37c161140942a13540b
Krishna Sharma <53052253+delta7-138@users.noreply.github.com>
2019-11-23 11:10:19 +0530

    Updation od addstudent.c
    Moving check_uniq function to util1.c

--- addstudent.c
+++ addstudent.c
@@ -1,30 +1,10 @@
 #include <stdio.h>
-#include "search.h"
 #include<stdlib.h>
 #define LARGE 1000
-//Function to check uniqueness of roll number
-int check_uniq(FILE *fp , int *roll_no){ //Accepting file pointer and roll no as parameters
-  char namef[LARGE] , namel[LARGE] , date[20], BUFFER[LARGE];
-  #include<stdio.h>
-  int *roll , *fees;
-  roll = (int *)malloc(sizeof(int));
-  fees = (int *)malloc(sizeof(int));
-  while(1){
-    search(namef,namel,roll,fees,date,fp); //Searching a particular row of a database using search function declared in search.h
-    if(feof(fp)) //When cursor reaches end of File
-      break;
 
-    if(*roll==*roll_no) //If roll number is not unique
-      return 0;
-
-    fgets(BUFFER , LARGE , fp); //To get to next Line
-  }return 1;
-}
-void add_student(FILE *fp , int roll)//this function will add the details of one student in the database and accepts file pointer as parameter
+void add_student(FILE *fp , int roll , char category[])//this function will add the details of one student in the database and accepts file pointer as parameter
 {
-    char fname[80],lname[80],category[20];
-    scanf("%s",date);
-    strcpy(category , "student");
+    char fname[80],lname[80];
     printf("Enter %s's first name...\n" ,  category);//entering student's first name
     scanf("%s",fname);
     printf("Enter %s's last name...\n" , category);//entering student's last name
@@ -34,6 +14,7 @@ void add_student(FILE *fp , int roll)//this function will add the details of one
     scanf("%d",&due);
     char date[20];
     printf("Enter the due date in DD-MM-YYYY format...\n");//entering the due date
+    scanf("%s",date);
     if(fp==NULL)
     {
         printf("Error opening File! Please try again...\n");//integrity check
@@ -41,7 +22,7 @@ void add_student(FILE *fp , int roll)//this function will add the details of one
     }
     else
     {
-        fprintf(fp,"namef=%s namel=%s roll=%d fees=%d date=%s\n",fname,lname,roll,due,date);//writing data into file
+        fprintf(fp,"namef=%s namel=%s roll=%d fees=%d date=%s category=%s\n",fname,lname,roll,due,date, category);//writing data into file
 
 
     }
