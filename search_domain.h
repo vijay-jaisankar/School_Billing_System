d2107a6d9ab528c7c7bb6d7783c936e939bad5d7
Krishna Sharma <53052253+delta7-138@users.noreply.github.com>
2019-11-24 13:55:09 +0530

    Update search_domain.h

--- search_domain.h
+++ search_domain.h
@@ -1,3 +1,3 @@
 void domain_search(char namef[],char namel[],int * roll,int * fees, char date[],char category[] , FILE* db);
-void domain_call_search(char key[], char value_lb,char value_ub,char namef[],char namel[],int * roll,int * fees, char date[],char category[] , int int_value_lb,int int_value_ub,int using,FILE* db);
+void domain_call_search(char key[], char value_lb,char value_ub,char namef[],char namel[],int * roll,int * fees, char date[],char category[] , int int_value_lb,int int_value_ub,char date_lb[] , char date_ub[] , int using,FILE* db);
 void search_domain_driver(FILE* db);
